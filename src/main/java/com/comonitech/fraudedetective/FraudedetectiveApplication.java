4dc3850fb25642cc913cd3d70017acacd75e3083
Ali Houssene <silahi.alihoussene@baamtu.com>
2023-09-21 14:49:46 +0000

    initialisation du projet

---
+++ src/main/java/com/comonitech/fraudedetective/FraudedetectiveApplication.java
@@ -0,0 +1,13 @@
+package com.comonitech.fraudedetective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudedetectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudedetectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comonitech/fraudedetective/FraudedetectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comonitech.fraudedetective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudedetectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
